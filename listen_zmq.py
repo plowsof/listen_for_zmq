098213fb0871d73219ab7cf84668f4ba93694aac
plowsof <77655812+plowsof@users.noreply.github.com>
2022-06-06 02:07:15 +0100

    Create listen_zmq.py

---
+++ listen_zmq.py
@@ -0,0 +1,40 @@
+import sys
+import zmq
+import time
+import threading
+import time
+
+node_list = ["node.community.rino.io","192.168.1.68"]
+
+port = "18083"
+
+class CountdownTask:
+    def __init__(self):
+        self._running = True
+      
+    def terminate(self):
+        self._running = False
+          
+    def run(self, node):
+        while self._running:
+            print(node)
+            context = zmq.Context()
+            socket = context.socket(zmq.SUB)
+            socket.setsockopt_string(zmq.SUBSCRIBE, '')
+            socket.setsockopt(zmq.CONFLATE, 1)  # last msg only.
+            socket.connect(f"tcp://{node}:{port}")  # must be placed after above options.
+            data = socket.recv()
+            print(data)
+            self._running = False
+      
+for node in node_list:
+    c = CountdownTask()
+    t = threading.Thread(target = c.run, args =(node, ))
+    t.start()
+    # wait 30 seconds for the thread to finish its work
+    t.join(30)
+    if t.is_alive():
+        print("The server did not publish a zmq message after 30 seconds")
+        c.terminate() 
+    else:
+        print("The server has a zmq port open :)")

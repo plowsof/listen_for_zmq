7ba84720ed6e846b90d35197d0a4c83c99a8fb74
plowsof <77655812+plowsof@users.noreply.github.com>
2022-06-06 03:25:14 +0100

    Update listen_zmq.py

--- listen_zmq.py
+++ listen_zmq.py
@@ -2,7 +2,9 @@ import sys
 import zmq
 import threading
 import os
-
+import requests
+from bs4 import BeautifulSoup
+import time
 node_list = ["node.community.rino.io","192.168.1.68"]
 
 port = "18083"
@@ -22,20 +24,45 @@ class CountdownTask:
             socket.setsockopt_string(zmq.SUBSCRIBE, '')
             socket.setsockopt(zmq.CONFLATE, 1)  # last msg only.
             socket.connect(f"tcp://{node}:{port}")  # must be placed after above options.
-            data = socket.recv()
-            print(data)
+            
+            while True:
+                data = socket.recv()
+                print(data)
+                break
             self._running = False
-      
-for node in node_list:
+
+def check_zmq(node):
     c = CountdownTask()
     t = threading.Thread(target = c.run, args =(node, ))
     t.start()
     # wait 30 seconds for the thread to finish its work
-    t.join(30)
+    t.join(5)
     if t.is_alive():
         print("The server did not publish a zmq message after 30 seconds")
         c.terminate() 
     else:
         print("The server has a zmq port open :)")
         c.terminate()
+
+def check_monero_fail():
+    response = requests.get("https://monero.fail/?nettype=mainnet")
+    webpage = response.content
+    stagenet = []
+    soup = BeautifulSoup(webpage, "html.parser")
+    for tr in soup.find_all('tr'):
+        values = [data for data in tr.find_all('td')]
+        for value in values:
+            if "http" in value.text:
+                if "onion" not in value.text:
+                    hostname = str(value.text)
+                    hostname = hostname.split("//")[1]
+                    if ":" in hostname:
+                        hostname = hostname.split(":")[0]
+                    stagenet.append(hostname)
+    for node in stagenet:
+        check_zmq(node)
+
+check_monero_fail()
 os._exit(1)
+
+#node.cryptocano.de
